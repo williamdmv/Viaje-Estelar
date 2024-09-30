70fff34d98bcfc21f5db019fc94b4abde0b21219
sebasVidal2002 <100444818+sebasVidal2002@users.noreply.github.com>
2024-09-30 11:34:57 -0500

    historia-122-123-133
    Se agregó:
    -Entorno base de la nave
    -Modelo 3D de una pelota de tenis
    -Modelos 3D de botones A,B,C,D.

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TouchPadTouch
+        {
+            get
+            {
+                return SteamVR_Actions.default_TouchPadTouch;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
